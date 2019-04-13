9d44257041beeb74be7679f963351d1fb7d69644
Shirly Niego <shirlyn995@gmail.com>
2019-04-13 15:02:29 +0300

    Initial commit

---
+++ ml-server/ml-server.js
@@ -0,0 +1,128 @@
+// Input data should look like:
+// {
+//   tweets: [
+//     {
+//       id: "asdasd",
+//       text: "i love israel"
+//     },
+//     {
+//       id: "asdcascasv",
+//       text: "bomb israel"
+//     }
+//   ]
+// }
+
+// Output data should look like:
+// {
+//   tweets: [
+//     {
+//       id: "asdasd",
+//       tag: "1.0"
+//     },
+//     {
+//       id: "asdcascasv",
+//       text: "-1.0"
+//     }
+//   ]
+// }
+
+const { exec } = require('child_process');
+const http = require('http');
+
+var modelScript = "model.py";
+
+var processedTweets = 0;
+var requiredTweets = 2;
+outputTweets = [];
+
+// TODO: Run in loop every X seconds
+http.get('http://localhost:8080/api/feed/GetRawFeedCount?limit=0', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    requiredTweets = data.tweets.length;
+    tagTweets(data.tweets);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+
+function tagTweets(inputTweetsArray) {
+  // Go over all of the tweets
+  for (var i = 0; i < inputTweetsArray.length; i++) {
+      (function(i){
+        // Run the model in new python child process
+        exec('python -W ignore ' + modelScript + ' "' + inputTweetsArray[i].text + '"', (err, stdout, stderr) => {
+          // Mark this tweet as processed
+          processedTweets++;
+          
+          if (err) {
+            console.error(`exec error: ${err}`);
+            return;
+          }
+
+          // Get only the tag number
+          stdout = stdout.substring(0, stdout.length - 2);
+
+          outputTweets.push({
+            id: inputTweetsArray[i].id,
+            tag: stdout
+          });
+
+          // Check if we have finished with all of the tweets we need to tag
+          checkIfFinished();
+        });
+      })(i)               
+  }
+}
+
+function checkIfFinished() {
+  if (processedTweets >= requiredTweets) {
+    console.log(outputTweets);
+    processedTweets = 0;
+    requiredTweets = 2;
+
+    // Send the tagged tweets back to the DB
+    sendTags(outputTweets);
+  }
+}
+
+function sendTags(outputTweetsArray) {
+    const options = {
+        hostname: '',
+        port: 3000,
+        path: '/api/feed/setscore',
+        method: 'POST'
+      };
+    
+    const req = http.request(options, (resp) => {
+        console.log('Sent tagged tweets');
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+      });
+    
+    req.write(JSON.stringify(outputTweetsArray));
+    req.end();
+}
+
+// For testing
+// tagTweets([
+//   {
+//     id: 111,
+//     text: "bomb israel"
+//   }, 
+//   {
+//     id: 222,
+//     text: "i love israel"
+//   }
+// ]);
---
+++ ml-server/model.py
@@ -0,0 +1,154 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import pickle
+
+class Vocab:
+    def __init__(self):
+        self.word_count = 0
+        self.ind2word = {}
+        self.word2ind = {}
+        
+        self.tag_count = 0
+        self.tag2id = {}
+        self.id2tag = {}
+    
+    # Gets a sentence and indexes each word in it
+    def index_document(self, document):
+        indexes = []
+        for token in document:
+          result = self.index_token(token)
+          if(result != None):
+            indexes.append(result)
+        return indexes
+
+    # Gets a word, and only if it wasn't previously indexed, it indexes it
+    # in two arrays
+    def index_token(self, token):
+        if token in self.word2ind:
+          return self.word2ind[token]
+          #self.word2ind[token] = self.word_count
+          #self.ind2word[self.word_count] = token
+          #self.word_count += 1
+    
+    # For each tag (Positive, negative, neutral) - returns an id (0, 1, 2)
+    def get_tag_id(self, tag):
+        if tag not in self.tag2id:
+            self.tag2id[tag] = self.tag_count
+            self.id2tag[self.tag_count] = tag
+            self.tag_count += 1
+        return self.tag2id[tag]
+		
+
+class Attn(nn.Module):
+    def __init__(self, hidden_size):
+        super(Attn, self).__init__()
+        
+        self.hidden_size = hidden_size
+        
+        # The linear layer's input is 2*hidden, and the output is 2*hidden
+        self.lin = nn.Linear(self.hidden_size*2, hidden_size*2)
+        
+        # The weight vector is 2*hidden
+        self.weight_vec = nn.Parameter(torch.FloatTensor(1, hidden_size*2))
+
+    def forward(self, outputs):
+        seq_len = len(outputs)
+        
+        # Zeorizing the energies
+        attn_energies = torch.zeros(seq_len)
+
+        # For every token in the tweet
+        for i in range(seq_len):
+            attn_energies[i] = self.score(outputs[i])
+
+        # All the energies    
+        return F.softmax(attn_energies).unsqueeze(0).unsqueeze(0)
+    
+    def score(self, output):
+      
+        # Feeding the linear layer with one token from the whole tweet
+        energy = self.lin(output)
+        energy = torch.dot(self.weight_vec.view(-1), energy.view(-1))
+        return energy	
+
+
+class EmoModel(nn.Module):
+    def __init__(self, input_size, hidden_size, embedding_size, output_size, n_layers):
+        super(EmoModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        
+        # Gets the word count and the embedding size
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        
+        # Input size - embedding size. Output size - hidden size.
+        self.lstm = nn.LSTM(embedding_size, hidden_size, n_layers, bidirectional=True)
+        self.out = nn.Linear(hidden_size*2, output_size)
+        self.attn = Attn(hidden_size)
+    
+    def forward(self, input_text):
+      
+        # The number of words in the current tweet.
+        seq_len = len(input_text.data)
+        
+        embedded_words = self.embedding(input_text).view(seq_len, 1, -1)
+
+        # Zeorizing
+        last_hidden = self.init_hidden()
+        
+        # Going through bi-directional LSTM
+        # Returns RNN_outputs (tweet length, 1 batch, 2*hidden size)
+        rnn_outputs, hidden = self.lstm(embedded_words, last_hidden)
+        
+        # Attention model on the outputs of the LSTM
+        # Returns the weights.
+        attn_weights = self.attn(rnn_outputs)
+        
+        # Reshaping
+        attn_weights = attn_weights.squeeze(1).view(seq_len, 1)
+        
+        # Removing the batch dimension (tweet length, 2*hidden size)
+        rnn_outputs = rnn_outputs.squeeze(1)
+        
+        attn_weights = attn_weights.expand(seq_len, self.hidden_size*2)
+        
+        # Multiplying the energies with the LSTM outputs
+        weigthed_outputs = torch.mul(rnn_outputs, attn_weights)
+
+        # Reducing dimensions. From (tweet lengt, 2*hidden size) to (2*hidden size) 
+        # Summing the outputs after they were multiplied with the energies
+        output = torch.sum(weigthed_outputs, -2)
+
+        # Inserting the one output into another linear layer
+        output = self.out(output)
+
+        return output
+    
+    def init_hidden(self):
+        return (torch.zeros(self.n_layers*2, 1, self.hidden_size),
+                    torch.zeros(self.n_layers*2, 1, self.hidden_size))
+
+with open("vocab.pkl", 'rb') as input:
+	vocab = pickle.load(input)	
+
+	
+model = torch.load("trainedModel.pt", map_location='cpu')
+
+import sys
+sentence = sys.argv[1].split()
+indexes = vocab.index_document(sentence)
+#print(indexes);
+sentence = torch.tensor(indexes)
+output = model(sentence)
+max = output[0]
+maxID = 0
+if (output[1] > max):
+	max = output[1]
+	maxID = 1
+if (output[2] > max):
+	max = output[2]
+	maxID = 2
+
+print (vocab.id2tag[maxID])	
